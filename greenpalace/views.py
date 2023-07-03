0cf469f87a7ef4af73956e35b345308465059325
tiffytirop <tiffany.tirop@strathmore.edu>
2023-07-03 12:38:28 +0300

    views

--- greenpalace/views.py
+++ greenpalace/views.py
@@ -4,7 +4,9 @@ from item.models import Category,Item
 def index(request):
     items = Item.objects.filter(is_sold=False)[0:6]
     categories = Category.objects.all()
-    return render(request, 'greenpalace/index.html', 'categories': categories, 'items': items)
+    return render(request, 'greenpalace/index.html', 
+                  {'categories': categories, 
+                  'items': items})
 
 def contact(request):
     return render(request, 'greenpalace/contact.html')

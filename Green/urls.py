3cb92a7e6e96b9e6316096067bca58f7cbdbd03b
tiffytirop <tiffany.tirop@strathmore.edu>
2023-07-03 14:12:44 +0300

    Category and Items

--- Green/urls.py
+++ Green/urls.py
@@ -13,7 +13,10 @@ Class-based views
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
@@ -22,4 +25,4 @@ urlpatterns = [
     path('', index, name='index'),
     path('contact/', contact, name='contact'),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

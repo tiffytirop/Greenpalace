cc05439ee69c0c9d24bd1e7a21a636f365767292
tiffytirop <tiffany.tirop@strathmore.edu>
2023-07-03 12:32:12 +0300

    Add Items

--- greenpalace/views.py
+++ greenpalace/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
-
+from item.models import Category,Item
 # Create your views here.
 def index(request):
-    return render(request, 'greenpalace/index.html')
+    items = Item.objects.filter(is_sold=False)[0:6]
+    categories = Category.objects.all()
+    return render(request, 'greenpalace/index.html', 'categories': categories, 'items': items)
 
 def contact(request):
     return render(request, 'greenpalace/contact.html')

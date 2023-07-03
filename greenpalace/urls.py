37d3af180564dc3744800780ac6e110fac438ef5
tiffytirop <tiffany.tirop@strathmore.edu>
2023-07-03 17:11:24 +0300

    Login form

--- greenpalace/urls.py
+++ greenpalace/urls.py
@@ -1,10 +1,14 @@
+from django.contrib.auth import views as auth_views
 from django.urls import path
-from .import views
 
-app_name ='greenpalace'
+from . import views
+from .forms import LoginForm
+
+app_name = 'greenpalace'
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('contact/', views.contact, name='contact'),
     path('signup/', views.Signup, name='signup'),
+    path('login/', auth_views.LoginView.as_view(template_name='greenpalace/login.html', authentication_form=LoginForm), name='login'),
 ]

3cbf3ba6a16a619f54d4cdc5d8fdafb93c692cc7
tiffytirop <tiffany.tirop@strathmore.edu>
2023-07-03 15:36:19 +0300

    related items and details page

--- Green/urls.py
+++ Green/urls.py
@@ -18,11 +18,12 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 
 from greenpalace.views import contact, index
 urlpatterns = [
     path('', index, name='index'),
+    path('items/', include('item.urls')),
     path('contact/', contact, name='contact'),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

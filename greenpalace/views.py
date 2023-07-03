68b2c56f36f7faee0b492d4c610711d16b054bb8
tiffytirop <tiffany.tirop@strathmore.edu>
2023-07-03 16:57:53 +0300

    signup form

--- greenpalace/views.py
+++ greenpalace/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from item.models import Category,Item
 
 from .forms import SignupForm
@@ -15,7 +15,12 @@ def contact(request):
     return render(request, 'greenpalace/contact.html')
 
 def Signup(request):
-    form=SignupForm()
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+
+        return redirect('/login/')
+    else:
+        form=SignupForm()
 
     return render(request, 'greenpalace/signup.html',{
         'form': form

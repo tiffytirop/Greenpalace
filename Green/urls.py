5b0e9bca303e56781190a30b10e0c59386e0fc48
tiffytirop <tiffany.tirop@strathmore.edu>
2023-07-03 16:35:10 +0300

    Sign up

--- Green/urls.py
+++ Green/urls.py
@@ -20,10 +20,9 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path,include
 
-from greenpalace.views import contact, index
+
 urlpatterns = [
-    path('', index, name='index'),
+    path('', include('greenpalace.urls')),
     path('items/', include('item.urls')),
-    path('contact/', contact, name='contact'),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- greenpalace/views.py
+++ greenpalace/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from item.models import Category,Item
+
+from .forms import SignupForm
+
 # Create your views here.
 def index(request):
     items = Item.objects.filter(is_sold=False)[0:6]
@@ -10,3 +13,10 @@ def index(request):
 
 def contact(request):
     return render(request, 'greenpalace/contact.html')
+
+def Signup(request):
+    form=SignupForm()
+
+    return render(request, 'greenpalace/signup.html',{
+        'form': form
+    } )

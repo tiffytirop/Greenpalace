db8b81b6750c03a8106727f938f074cb039b6be7
tiffytirop <tiffany.tirop@strathmore.edu>
2023-07-13 11:08:53 +0300

    Adding Items

--- item/views.py
+++ item/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render,get_object_or_404
 
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render,get_object_or_404,redirect
+
+from .forms import NewItemForm
 from .models import Item
 
 # Create your views here.
@@ -11,3 +14,21 @@ def detail(request, pk):
     'item':item,
     'related-items': related_items
 })
+@login_required
+def new(request):
+    if request.method == 'POST':
+        form = NewItemForm(request.POST, request.FILES)
+
+        if form.is_valid():
+            item = form.save(commit=False)
+            item.created_by = request.user
+            item.save()
+
+            return redirect('item:detail', pk=item.id)
+        else:
+            form=NewItemForm
+
+    return render(request, 'item/form.html',{
+        'form':form,
+        'title': 'New Item'
+    })

47d040588f66e63ffa4e2d4de22e96ddc6014d0c
tiffytirop <tiffany.tirop@strathmore.edu>
2023-07-22 10:37:24 +0300

    initialize conversations

--- Green/settings.py
+++ Green/settings.py
@@ -41,8 +41,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'greenpalace',
+    'conversation',
     'dashboard',
-    'item'
+    'item',
 ]
 
 MIDDLEWARE = [
